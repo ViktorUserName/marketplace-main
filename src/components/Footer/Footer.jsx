eca171a41b694bb64d152ee17c46ed936fd77d09
Roman <100953835+Chezy13@users.noreply.github.com>
2022-09-19 20:45:12 +0300

    mobile v  corrections

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Footer.scss';
 import emeil from '../../img/emeillMG.svg';
 import phone from '../../img/phoneIMG.svg';
+import inst from "../../img/instagramICO.svg";
 
 
 
@@ -15,19 +16,26 @@ const Footer = () => {
                         <p className="footer__info-item-title">Contact information</p>
                         <div className='footer__info-item-label'>
                             <img src={emeil} alt="" className="footer__info-svg" />
-                            <p>foxwebduo@gmail.com</p>
+                            <p><a href="mailto:foxwebduo@gmail.com">foxwebduo@gmail.com</a></p>
                         </div>
                         <div className='footer__info-item-label'>
                             <img src={phone} alt="" className="footer__info-svg" />
-                            <p>+375256263144 (life)</p>
+                            <p><a href="tel:+375256263144">+375256263144</a></p>
                         </div>
                     </div>
 
                     <div className="footer__info-Socials">
-                        <h1 className="footer__info-item-title">Socials</h1>
-                        <p>Instagram</p>
-                        <a href='https://instagram.com/ch4zc'>@ch4zc</a>
-                        <a href='https://instagram.com/lobster_piza'>@lobster_piza</a>
+                        <p className="footer__info-item-title">Socials</p>
+                        <div className='footer__info-item-label'>
+                            <img src={inst} alt="instagramm" />
+                            <a href='https://instagram.com/ch4zc'>@ch4zc</a>
+                        </div>
+                        <div className='footer__info-item-label'>
+                            <img src={inst} alt="instagramm" />
+                            <a href='https://instagram.com/lobster_piza'>@lobster_piza</a>
+                        </div>
+                        
+                        
                     </div>
                 </div>
                 <p className="footer-last">(с) 2022. All Rights Reserved.</p>
