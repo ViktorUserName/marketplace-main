ccf2f84caeff5d3bb70c9efa9855243e2abca0c5
Roman <100953835+Chezy13@users.noreply.github.com>
2022-09-27 19:47:21 +0300

    slider work

--- src/components/Specification/Slider.jsx
+++ src/components/Specification/Slider.jsx
@@ -1,67 +1,25 @@
 import React from 'react';
 import "./slider.scss"
+
 import { Navigation, Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
+
 import 'swiper/scss';
 import 'swiper/scss/navigation';
 import 'swiper/scss/pagination';
+
 import img1 from "../../img/examples/coffee.png";
 import img2 from "../../img/examples/hause.png";
 import img3 from "../../img/examples/taste-eat.png";
 
-// Swiper.use([Navigation]);
-// Swiper.use([Pagination]);
-
-// new Swiper('.swiper', {
-//     navigation : {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev',
-//     },
-//     pagination: {
-//         el: '.swiper-pagination',
-//         clickable: 'true',
-//         dynamicBullets: 'true',
-//     }
-// });
-
 const Slider = () => {
     return (
-        // <div className='wrapper'>
-        //     <div className='swiper'>
-            
-        //         <div className="swiper-wrapper">
-        //             <div className="swiper-slide">
-        //                 <div className="slider-image">
-        //                     <img src={img1} alt="" className="" />
-        //                     <h1 className="web-info">Lorum ipsum</h1>
-        //                 </div>
-        //             </div>
-        //             <div className="swiper-slide">
-        //                 <div className="slider-image">
-        //                     <img src={img2} alt="" className="" />
-        //                     <h1 className="web-info">Lorum ipsum</h1>
-        //                 </div>
-        //             </div>    
-        //             <div className="swiper-slide">
-        //                  <div className="slider-image">
-        //                     <img src={img3} alt="" className="" />
-        //                     <h1 className="web-info">Lorum ipsum</h1>
-        //                 </div>
-        //             </div> 
-        //          </div>
-        //          <div class="swiper-pagination size-pag"></div>
-        //          <div class="swiper-button-prev"></div>
-        //          <div class="swiper-button-next"></div>
-        //     </div>
-
-        // </div>
         <div className='wrapper'>
             <Swiper 
             spaceBetween={50} 
             modules={[Navigation, Pagination]}
             navigation
             pagination={{ clickable: true }}
-
             >
             <SwiperSlide>
                     <a href="https://coffee-fox.netlify.app/"  target='_blank' className="slider-a slider-image">
--- src/components/Specification/Specification.jsx
+++ src/components/Specification/Specification.jsx
@@ -1,51 +1,24 @@
 import React from 'react';
-import "./Specification.scss"
-import SpecImg from '../../img/SpecificationIMG.png'
 import Slider from './Slider';
+import "./Specification.scss"
 import "./slider.scss";
 
 
 const Specification = () => {
     return (
         <section className='specification' id='ourWork'>
-
-        <div className="specification__present">
-            <div className="wrapper">    
-                <div className="specification__present-content">
-                        <h1>
-                        Examples of our projects
-                        </h1>
-                </div>
-            </div>
-        </div>
-
-        <div className="specification__examples">
-        <Slider/>
-        </div>
-        {/* <div className="specification__examples">
-            <div className="wrapper">
-                <div className="specification__examples-items">
-                    <div className="specification__examples-item">
-                        <img src={SpecImg} alt=""/>
-                        <h1>01. Исследование и упаковка</h1>
-                        <p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века.</p>
-                        <a href="#">Подробнее</a>
-                    </div>
-                    <div className="specification__examples-item">
-                        <img src={SpecImg} alt=""/>
-                        <h1>01. Исследование и упаковка</h1>
-                        <p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века.</p>
-                        <a href="#">Подробнее</a>
-                    </div>
-                    <div className="specification__examples-item">
-                        <img src={SpecImg} alt=""/>
-                        <h1>01. Исследование и упаковка</h1>
-                        <p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века.</p>
-                        <a href="#">Подробнее</a>
+            <div className="specification__present">
+                <div className="wrapper">    
+                    <div className="specification__present-content">
+                            <h1>
+                            Examples of our projects
+                            </h1>
                     </div>
                 </div>
             </div>
-        </div> */}
+            <div className="specification__examples">
+                <Slider/>
+            </div>
         </section>
         
     );
